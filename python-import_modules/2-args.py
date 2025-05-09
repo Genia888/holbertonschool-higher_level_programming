a19faa16dafb97c7ad3c891e9fbcd2d5b8d75d2e
Genia888 <khalepo@gmail.com>
2025-05-09 15:53:56 +0200

    create 2 args file

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print(f"{len(argv) - 1} arguments.")
+    elif len(argv) == 2:
+        print(f"{len(argv) - 1} argument:")
+    else:
+        print(f"{len(argv) - 1} arguments:")
+    for i in range(len(argv) - 1):
+        print(f"{i + 1}: {argv[i + 1]}")
