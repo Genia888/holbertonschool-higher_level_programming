8c787a3bb0d201170cd53e7ad3f36185d4d76b33
Genia888 <khalepo@gmail.com>
2025-05-15 16:56:27 +0200

    Refactor list_division function for clarity and add new test script for user input

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
+
     for i in range(list_length):
         try:
-            result.append(my_list_1[i] / my_list_2[i])
+            a = my_list_1[i]
+            b = my_list_2[i]
+            result.append(a / b)
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
-        except TypeError:
+        except (TypeError, ValueError):
             print("wrong type")
             result.append(0)
         except IndexError:
             print("out of range")
-            break
+            result.append(0)
+        finally:
+            pass
+
     return result
