a1e72dec70b0138bfc386a141d0c7632130e7920
Genia888 <khalepo@gmail.com>
2025-05-16 21:37:44 +0200

    Update docstring formatting in text_indentation function for clarity

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def text_indentation(text):
     """
-    This function prints a text with 2 new lines after each of these characters: ., ? and :
+    This function prints a text with 2 new lines
+    after each of these characters: ., ? and :
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    
     i = 0
     while i < len(text):
         print(text[i], end="")
