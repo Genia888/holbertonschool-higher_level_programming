ebe4d0235759854f0b2bdecc0204361d701dcb3b
Genia888 <khalepo@gmail.com>
2025-05-17 00:10:26 +0200

    Refactor text_indentation function for improved readability and efficiency; update test cases for consistency

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 def text_indentation(text):
+    '''Function that print /n after '.' '?' ':'''
+
+    new_text = ""
+    i = 0
+    delim = ['?', '.', ':']
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    new = ""
+    while i < len(text):
+        new_text += text[i]
+        if text[i] in delim:
+            new_text += "\n\n"
+            i += 1
+            while (i < len(text) and text[i] == ' '):
+                i += 1
+            continue
+        i += 1
 
-    for i in text:
-        new += i
-        if i in ".:?":
-            print("{}".format(new.strip()), end="\n\n")
-            new = ""
-
-    if new.strip() != "":
-        print("{}".format(new.strip()), end="")
+    print("{:s}".format(new_text), end="")
