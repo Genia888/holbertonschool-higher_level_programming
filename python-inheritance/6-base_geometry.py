8d16a6e3fa9637a72ef12be78bdf925139ee642f
Genia888 <khalepo@gmail.com>
2025-05-26 17:03:37 +0200

    Add BaseGeometry class as a foundational structure for geometry-related classes

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
