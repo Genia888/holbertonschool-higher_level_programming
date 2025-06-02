1aef2720bd853a39185e64a658f3cd9fdbb6e5f5
Genia888 <khalepo@gmail.com>
2025-06-02 10:54:17 +0200

    Refactor docstrings and improve integer validation in BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""A class BaseGeometry"""
+"""Defines a base geometry class."""
 
 
 class BaseGeometry:
     """Base class for geometry."""
 
     def area(self):
+        """Raises an exception for unimplemented area."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+        """Validates that value is a positive integer."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
