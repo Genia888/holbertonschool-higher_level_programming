637be36ef30223ee6ff53d2c57464c4bf66ff92d
Genia888 <khalepo@gmail.com>
2025-06-06 11:43:37 +0200

    Add CustomObject class with pickle serialization and deserialization methods

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Custom class with pickle serialization and deserialization"""
+
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except Exception:
+            pass  # Fail silently as instructed
+
+    @classmethod
+    def deserialize(cls, filename):
+
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except Exception:
+            return None
