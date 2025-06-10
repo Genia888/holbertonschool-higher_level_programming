4387141ffc007c7c61557f9122b258c2f764c7cf
Genia888 <khalepo@gmail.com>
2025-06-10 14:23:19 +0200

    Improve code formatting and readability in task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,12 +1,13 @@
 import requests
 import csv
 
+
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    
+
     print("Status Code:", response.status_code)
-    
+
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
@@ -14,18 +15,22 @@ def fetch_and_print_posts():
     else:
         print("Error fetching the data.")
 
+
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
 
     if response.status_code == 200:
         posts = response.json()
-        
+
         data = []
         for post in posts:
-            data.append({"id": post["id"], "title": post["title"],  "body": post["body"]})
+            data.append(
+                {"id": post["id"], "title": post["title"],
+                 "body": post["body"]})
 
-        with open("posts.csv", mode="w", newline='', encoding="utf-8") as csvfile:
+        with open("posts.csv", mode="w", newline='',
+                  encoding="utf-8") as csvfile:
             fieldnames = ["id", "title", "body"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
