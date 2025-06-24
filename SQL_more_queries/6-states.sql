6ce1bbf1f66d9d0bf9c6400b9ea545ff5c2fb085
Genia888 <khalepo@gmail.com>
2025-06-24 17:26:13 +0200

    Update comments in 6-states.sql for clarity and add 7-cities.sql table creation script

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,10 +1,10 @@
--- Créer la base de données si elle n'existe pas
+-- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Utiliser cette base
 USE hbtn_0d_usa;
 
--- Créer la table states si elle n'existe pas
+-- Use this base
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name VARCHAR(256) NOT NULL
