d9121d2914ebe48281664b6160562b44769fb7cc
Genia888 <khalepo@gmail.com>
2025-06-28 00:47:35 +0200

    Refactor 7-model_state_fetch_all.py for clarity and remove redundant main function; improve docstring in model_state.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
+'''List all States'''
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: ./7-model_state_fetch_all.py <mysql username> <mysql password> <database name>")
-        return
-
-    # Collect arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create an engine that connects to the MySQL database
-    engine = create_engine(f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/{database_name}', pool_pre_ping=True)
-
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query all states, sorted by 'id'
-    states = session.query(State).order_by(State.id).all()
-
-    # Print results in the requested format
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    # Close the session
-    session.close()
 
 if __name__ == '__main__':
-    main()
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
