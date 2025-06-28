a19e248c1e0e3301dcf6f82a1f91df77140ccf2e
Genia888 <khalepo@gmail.com>
2025-06-28 16:09:10 +0200

    Refactor 9-model_state_filter_a.py for improved readability and clarity in docstring and code structure

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-'''List all name with 'a' inside in he list order by state_id'''
+"""List all State objects with 'a' in the name, ordered by state.id."""
 
-
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
-
 
 if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <username> <password> <database>")
+        sys.exit(1)
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1],
-                                   sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id)\
-            .filter(State.name.like('%a%')):
-        print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+        print(f"{state.id}: {state.name}")
+
     session.close()
